adcf189c483a499014c8c0512eb50eab89e6b092
Mario <mario.rodavila@gmail.com>
2019-01-26 01:40:32 +0100

    Controller

--- Source/KHAGANDOVOY/KHAGANDOVOY.h
+++ Source/KHAGANDOVOY/KHAGANDOVOY.h
@@ -4,4 +4,12 @@
 
 #include "CoreMinimal.h"
 
+
+#define Print(String,Time, Color); GEngine->AddOnScreenDebugMessage(-1, Time, Color,String);
+#define RED		FColor::Red
+#define BLUE	FColor::Blue
+#define GREEN	FColor::Green
+#define YELLOW	FColor::Yellow
+
+
 DECLARE_LOG_CATEGORY_EXTERN(LogKHAGANDOVOY, Log, All);
