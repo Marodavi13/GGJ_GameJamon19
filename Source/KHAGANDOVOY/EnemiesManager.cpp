25dc95d031c38478a24e9329f0b2efdeed0c39bf
Mario <mario.rodavila@gmail.com>
2019-01-27 01:12:35 +0100

    ENEMY MANAYEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEER

---
+++ Source/KHAGANDOVOY/EnemiesManager.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EnemiesManager.h"
+#include "KhagarPoint.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+// Sets default values
+AEnemiesManager::AEnemiesManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemiesManager::BeginPlay()
+{
+	Super::BeginPlay();
+	TArray<AActor*> KhagarPoints;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AKhagarPoint::StaticClass(), KhagarPoints);
+	for (AActor* Point : KhagarPoints)
+	{
+		AKhagarPoint* KP = Cast<AKhagarPoint>(Point);
+
+		switch (KP->TargetPointType)
+		{
+		case ETargetPoint::BARRIER:
+			BarrierPoints.AddUnique(KP);
+			break;
+		case ETargetPoint::DOG:
+			DogPoints.AddUnique(KP);
+			break;
+		case ETargetPoint::TRUNK:
+			TrunkPoints.AddUnique(KP);
+		case ETargetPoint::UFO:
+			UFOPoints.AddUnique(KP);
+			break;
+		default: break;
+		}
+	}
+	MoveTrunks();
+	SpawnEnemies();
+}
+
+// Called every frame
+void AEnemiesManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/KHAGANDOVOY/KhagarPoint.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TargetPoint.h"
+#include "Runtime/Engine/Classes/Components/ArrowComponent.h"
+#include "KhagarPoint.generated.h"
+
+UENUM(BlueprintType)
+enum class ETargetPoint : uint8 {
+	NONE,
+	TRUNK,
+	BARRIER,
+	UFO,
+	DOG
+};
+
+
+/**
+ * 
+ */
+UCLASS()
+class KHAGANDOVOY_API AKhagarPoint : public ATargetPoint
+{
+	GENERATED_BODY()
+public:
+	AKhagarPoint();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		ETargetPoint TargetPointType;
+
+};
